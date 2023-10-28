2d1ea51070c71dc7a49a3966070c5fea8ceca0b8
Ryan Hood <ryanhood@ryans-mbp.lan>
2023-10-28 18:41:22 -0500

    deploying

---
+++ src/components/Instagram.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Instagram = () => {
+  return (
+    <div className="w-full bg-white py-16 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 space-x-5 space-y-5">
+    <p>hello world</p>
+      </div>
+    </div>
+  );
+};
+
+export default Instagram;
