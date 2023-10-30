24eec52ce47c45ed22980e3785cc5292a9dc3990
Ryan Hood <ryanhood@Ryans-MacBook-Pro.local>
2023-10-30 01:13:25 -0500

    twitter api call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import Instagram from './components/Instagram';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from './components/Navbar';
+import TweetFeed from "./components/TweetFeed";
+import RuleList from "./components/RuleList";
 
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <div className="introduction"></div>
 
-function App() {
-  return (
-    <div className="App">
-      <Instagram />
+        <h1 className="ui header">
+          <div className="content">
+            Real Time Tweet Streamer
+            <div className="sub header">Powered by Twitter data</div>
+          </div>
+        </h1>
 
-    </div>
-  );
+        <div className="ui container">
+          <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<RuleList />} />
+              <Route path="/rules" element={<RuleList />} />
+              <Route path="/tweets" element={<TweetFeed />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
